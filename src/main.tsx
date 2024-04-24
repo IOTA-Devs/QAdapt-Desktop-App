db345725411338d85fd8b5b92862159737d91d09
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2024-04-24 01:42:28 -0600

    Implemented sign in page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import "./index.scss";
+
+const Signin = lazy(() => import("./pages/signin"));
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Signin/>
+    }
+  ]);
+
+  return (
+    <div className="flex justify-center flex-col min-h-screen">
+      <Suspense>
+        <RouterProvider router={router} />
+      </Suspense>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
