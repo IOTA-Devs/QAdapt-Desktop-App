f4946ba8ea35f599cf5d03a7d1e03abe5b082549
Sebastian Ramirez <sebastian@Sebastians-MacBook-Air-6.local>
2024-05-03 06:57:38 -0600

    collections table started and imported

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -1,7 +1,30 @@
+import {Payment, columns} from "../components/custom/columns"
+import {DataTable} from "../components/custom/data-table"
+
+async function getData(): Promise<Payment[]> {
+	return [
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "pending",
+			email: "m@example.com",
+		},
+	]
+}
+
 export default function Collections() {
+	//const data = await getData()
+	const data = [
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "pending",
+			email: "m@example.com",
+		},
+	]
     return(
-        <>
-            Collections Works!
-        </>
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={data} />
+        </div>
     );
 }
