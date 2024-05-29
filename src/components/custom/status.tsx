72277b6033cf21e84f7b3db9d24d702d3f4e3a48
CDavidSV <cdavidsv6@gmail.com>
2024-05-28 21:46:11 -0600

    Update status.tsx

--- src/components/custom/status.tsx
+++ src/components/custom/status.tsx
@@ -7,24 +7,29 @@ import {
 import { StatusComponentTypes } from "@/types/types";
 
 export default function Status({ status, message }: { status: StatusComponentTypes, message: string }) {
-    const getStatusColor = (status: StatusComponentTypes) => {
-        const colorStatusToHexMap = {
-            danger: "#FF3838",
-            warning: "#FFB302",
-            caution: "#FCE83A",
-            ok: "#56F000",
-            info: "#2DCCFF",
-            unavailable: "#A4ABB6"
+    const getStatusColor = (status: StatusComponentTypes, opacity: number) => {
+        const colorStatusRGB = {
+            danger: [255, 56, 56],
+            warning: [2545, 179, 2],
+            caution: [252, 232, 58],
+            ok: [86, 240, 0],
+            info: [45, 204, 255],
+            unavailable: [164, 171, 182]
         };
 
-        return colorStatusToHexMap[status];
+        const color = colorStatusRGB[status];
+        return `rgb(${color[0]}, ${color[1]}, ${color[2]}, ${opacity})`;
     }
     
     return (
         <TooltipProvider>
             <Tooltip>
                 <TooltipTrigger asChild>
-                    <div style={{ backgroundColor: getStatusColor(status) }} className="block rounded-full h-3 w-3 bg-opacity-50"></div>
+                    <div className="inline-block">
+                        <div style={{ backgroundColor: getStatusColor(status, 0.4), border: `1px solid ${getStatusColor(status, 1)}` }} className="flex items-center justify-center rounded-md px-3">
+                            <p className="font-semibold">{message}</p>
+                        </div>
+                    </div>
                 </TooltipTrigger>
                 <TooltipContent>
                     <p>{message}</p>
